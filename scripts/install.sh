7feabb58ad94700177a4444cee120c67e6702d5b
Jorge A. Reyes Jr. <10119138+jorgeareyesjr@users.noreply.github.com>
2025-02-20 17:49:12 -0800

    Update the install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,57 +5,92 @@
 # NOTE: This script relies on `pipenv run` instead of `pipenv shell` to avoid spawning a new subshell
 
 # The installation script will:
-# 1. Generate a local SSL certificate for your localhost/loopback IP address
+# 1. Create an `env` directory
 # 2. Initialize environment variables
-# 3. Install dependencies
-# 4. Initialize database migrations
+# 3. Generate a local SSL certificate for your localhost/loopback IP address
+# 4. Install dependencies
+# 5. Apply initial database migrations
 
-echo ""
-echo "Thank you for choosing the Django Data Relay!"
 echo ""
 echo "Starting installation..."
 echo ""
 
-# 1. Generate a local SSL certificate for your localhost/loopback IP address
-echo "Generating local SSL certificate..."
-mkcert -cert-file base/cert.pem -key-file base/key.pem localhost 127.0.0.1
+# 1. Create an `env` directory
+# DO NOT commit the `.env` file to a public repository, instead, add it to your `.gitignore` file.
+echo "Configuring environment..."
+echo ""
+mkdir -p env
 
 # 2. Initializing environment variables
-# `Pipenv` handles printing the output for this step
-ENV_FILE=".env"
-ENV_FILE_COMMENT="# The Django Data Relay relies on the following environment variables for it's configuration. For security purposes, DO NOT commit this file to a public repository. Instead, ensure it is included in your \`.gitignore\` file. These settings contain sensitive information and should be kept confidential.
+ENV_FILE="env/.env"
+ENV_FILE_COMMENT="# The environment variables in this file are required to properly configure the Django Data Relay.
+# For security purposes, DO NOT commit this file to a public repository, instead, ensure it is included in your \`.gitignore\` file.
+# These settings contain sensitive information and should be kept confidential.
+# Be aware, if you change any of these settings, you will need to restart the Django Data Relay's local server.
+# If you are using Docker, you will need to rebuild the Docker container.
 
-# Be aware, if you change any of these settings, you will need to restart the Django Data Relay local server. If you are using Docker, you will need to rebuild the Docker container.
+# Deployment checklist:
+# https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 "
-SECRET_KEY_TEXT="# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY_BLOCK="# SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY NOTE: use a long and complex key comprised of uppercase, lowercase, numbers, and special characters.
 SECRET_KEY='A-S4MPL3-S3CR3T-K3Y-C0MPRIS3D-0F-L0NG-C4R4CT3RS'
 "
-DEBUG_TEXT="# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG_BLOCK="# SECURITY WARNING: don't run with debug turned on in production!
 # SECURITY NOTE: the DEBUG setting should be set to a boolean value.
-DEBUG='true'
+DEBUG_KEY='true'
 "
-ALLOWED_HOSTS_TEXT="# SECURITY WARNING: don't expose your project to malicious requests!
+ALLOWED_HOSTS_BLOCK="# SECURITY WARNING: don't expose your project to malicious requests!
 # SECURITY NOTE: if DEBUG is set to \`false\`, ALLOWED_HOSTS requires a value.
-ALLOWED_HOSTS_LIST='localhost, 127.0.0.1, [::1]'
+ALLOWED_HOSTS='localhost, 127.0.0.1, [::1]'
 "
-# Combine the environment variables
+CSRF_COOKIE_SECURE_BLOCK="# SECURITY WARNING: don't let the CSRF cookie travel across insecure connections!
+# SECURITY NOTE: the CSRF_COOKIE_SECURE should be set to a boolean value.
+CSRF_COOKIE_SECURE = 'true'
+"
+SESSION_COOKIE_SECURE_BLOCK="# SECURITY WARNING: don't let the session cookie travel across insecure connections!
+# SECURITY NOTE: the SESSION_COOKIE_SECURE should be set to a boolean value.
+SESSION_COOKIE_SECURE = 'true'
+"
+SECURE_HSTS_SECONDS_BLOCK="# SECURITY WARNING: don't let the browser make insecure connections!
+# SECURITY NOTE: Subsequent visits "reset" the browser's HSTS header to the latest policy.
+SECURE_HSTS_SECONDS = '31536000' # 1 year.
+"
+SECURE_SSL_REDIRECT_BLOCK="# SECURITY WARNING: don't let connections redirect away from HTTPS.
+# SECURITY NOTE: if SECURE_SSL_REDIRECT is set to \`true\`, HTTP connections are permanently redirected to HTTPS.
+SECURE_SSL_REDIRECT = 'true'
+"
+
+# Combine the environment variable blocks
 ENV_TEXT_TO_ADD="$ENV_FILE_COMMENT
-$SECRET_KEY_TEXT
-$DEBUG_TEXT
-$ALLOWED_HOSTS_TEXT
-"
-# Write the environment variables to the .env file
+$SECRET_KEY_BLOCK
+$DEBUG_BLOCK
+$ALLOWED_HOSTS_BLOCK
+$CSRF_COOKIE_SECURE_BLOCK
+$SESSION_COOKIE_SECURE_BLOCK
+$SECURE_HSTS_SECONDS_BLOCK
+$SECURE_SSL_REDIRECT_BLOCK"
+
+# Write the environment variable blocks to the `.env` file
 echo "$ENV_TEXT_TO_ADD" > "$ENV_FILE"
 
+# 2. Generate a local SSL certificate for your localhost/loopback IP address
+echo "Generating local SSL certificate..."
+mkcert -cert-file env/cert.pem -key-file env/key.pem localhost 127.0.0.1
+# `mkcert` adds output
+
 # 3. Install dependencies
-# `Pipenv` handles printing the output for this step
+echo "Installing required packages..."
+echo ""
 pipenv install
+# `Pipenv` adds output
 echo ""
 
-# 4. Initialize database migrations
-# `Pipenv` handles printing the output for this step
+# 4. Apply initial database migrations
+echo "Applying initial database migrations..."
+echo ""
 pipenv run python base/manage.py migrate
+# `Pipenv` adds output
 echo ""
 
 echo "Installation complete!"
