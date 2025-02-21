d233687de668a4cbee344fe8798c77fc043befdd
Jorge A. Reyes Jr. <10119138+jorgeareyesjr@users.noreply.github.com>
2025-02-20 17:49:23 -0800

    Update the start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,29 +6,32 @@
 
 # The start script will:
 # 1. Apply environment variables
-# 2. Initialize environment variables
-# 3. Install dependencies
-# 4. Run the initial database migrations
+# 2. Apply database migrations
+# 3. Launch a secure development server
 
 echo ""
-echo "Initializing the Django Data Relay..."
+echo "Initializing..."
 echo ""
 
 # 1. Apply environment variables
-# `Pipenv` handles printing the output for this step
+echo "Configuring environment..."
+echo ""
 pipenv install
+# `Pipenv` adds output
 echo ""
 
-# 2. Run the database migrations
-# `Pipenv` handles printing the output for this step
+# 2. Apply database migrations
+echo "Applying database migrations..."
+echo ""
 pipenv run python base/manage.py migrate
+# `Pipenv` adds output
 echo ""
 
 # 3. Launch the secure development server
-echo "Launching the Django Data Relay..."
+echo "Launching the secure development server..."
 echo ""
-# `Pipenv` handles printing the output for this step
-pipenv run python base/manage.py runserver_plus --cert-file base/cert.pem --key-file base/key.pem
+pipenv run python base/manage.py runserver_plus --cert-file env/cert.pem --key-file env/key.pem
+# `Pipenv` adds output
 
 # End of script
 
